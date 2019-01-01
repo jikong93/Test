1d49c6992be2cc344330946fa6b9ff65a9880840
jikong93 <794472029@qq.com>
2019-01-01 16:22:03 +0800

    test4

--- src/main/java/com/jk/App.java
+++ src/main/java/com/jk/App.java
@@ -5,7 +5,7 @@ package com.jk;
  */
 public class App {
     public static void main(String[] args) {
-        String str = "test1";
+        String str = "test4";
         System.out.println("Hello World!");
 
 
