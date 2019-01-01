8885bf512e4b5e989b182df10906a65b5191e91a
jikong93 <794472029@qq.com>
2019-01-01 16:17:55 +0800

    test1

--- src/main/java/com/jk/App.java
+++ src/main/java/com/jk/App.java
@@ -2,12 +2,12 @@ package com.jk;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String str = "test1";
+        System.out.println("Hello World!");
+
+
     }
 }
